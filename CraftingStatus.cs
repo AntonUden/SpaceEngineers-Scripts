1d32bdf000055c12e765c98dd43033b2f44db93f
Anton udén <udenanton@hotmail.com>
2020-11-02 18:12:53 +0100

    Update CraftingStatus.cs

--- CraftingStatus.cs
+++ CraftingStatus.cs
@@ -1,45 +1,56 @@
-public void Main(string args) {
-  String[] argumentArray = args.Split(',');
+public void DisplayMessage(string message) {
+  IMyTextSurface localDisplay = Me.GetSurface(0);
+  localDisplay.ContentType = ContentType.TEXT_AND_IMAGE;
+  localDisplay.FontSize = 1;
+  localDisplay.WriteText(message);
+}
 
-  if (argumentArray.Length < 2) {
-    IMyTextSurface display = Me.GetSurface(0);
-    display.ContentType = ContentType.TEXT_AND_IMAGE;
-    display.FontSize = 1;
-    display.WriteText("Bad argument\narray size:\n" + argumentArray.Length);
-  } else {
+public List < IMyTextSurface > displays = new List < IMyTextSurface > ();
 
-    List < IMyAssembler > assemblers = new List < IMyAssembler > ();
-    GridTerminalSystem.GetBlocksOfType < IMyAssembler > (assemblers);
+public void Main(string args, UpdateType updateType) {
+  if ((updateType & (UpdateType.Trigger | UpdateType.Terminal)) != 0) {
+    String[] argumentArray = args.Split(',');
+    if (args.Length == 0) {
+      DisplayMessage("Please privide a comma separated list of\ndisplays to output to");
+      Runtime.UpdateFrequency = UpdateFrequency.None;
+      return;
+    } else {
+      displays = new List < IMyTextSurface > ();
 
-    int items = 0;
+      for (int i = 0; i < argumentArray.Length; i++) {
+        IMyTextSurface display = GridTerminalSystem.GetBlockWithName(argumentArray[i]) as IMyTextSurface;
 
-    foreach(IMyAssembler assembler in assemblers) {
-      List < MyProductionItem > queue = new List < MyProductionItem > ();
-      assembler.GetQueue(queue);
-
-      foreach(MyProductionItem item in queue) {
-        items += (int)(item.Amount.RawValue / 1000000);
+        displays.Add(display);
       }
+
+      Runtime.UpdateFrequency = UpdateFrequency.Update100;
     }
-
-    for (int i = 1; i < argumentArray.Length; i++) {
-      IMyTextSurface display = GridTerminalSystem.GetBlockWithName(argumentArray[i]) as IMyTextSurface;
-      display.ContentType = ContentType.TEXT_AND_IMAGE;
-      display.WriteText(items + "\nitems in queue");
-
-      if (items > 0) {
-        display.FontColor = Color.Yellow;
-      } else {
-        display.FontColor = Color.Green;
-      }
-    }
-
-    IMyTextSurface localDisplay = Me.GetSurface(0);
-    localDisplay.ContentType = ContentType.TEXT_AND_IMAGE;
-    localDisplay.FontSize = 1;
-    localDisplay.WriteText("ok\n" + items + "\nitems in queue");
-
-    IMyTimerBlock timer = GridTerminalSystem.GetBlockWithName(argumentArray[0]) as IMyTimerBlock;
-    timer.StartCountdown();
   }
+
+  List < IMyAssembler > assemblers = new List < IMyAssembler > ();
+  GridTerminalSystem.GetBlocksOfType < IMyAssembler > (assemblers);
+
+  int items = 0;
+
+  foreach(IMyAssembler assembler in assemblers) {
+    List < MyProductionItem > queue = new List < MyProductionItem > ();
+    assembler.GetQueue(queue);
+
+    foreach(MyProductionItem item in queue) {
+      items += (int)(item.Amount.RawValue / 1000000);
+    }
+  }
+
+  foreach(IMyTextPanel display in displays) {
+    display.ContentType = ContentType.TEXT_AND_IMAGE;
+    display.WriteText(items + "\nitems in queue");
+
+    if (items > 0) {
+      display.FontColor = Color.Yellow;
+    } else {
+      display.FontColor = Color.Green;
+    }
+  }
+
+  DisplayMessage("ok\n" + items + "\nitems in queue\n" + displays.Count + " displays\nUpdate type: " + Enum.GetName(typeof(UpdateType), updateType));
 }
